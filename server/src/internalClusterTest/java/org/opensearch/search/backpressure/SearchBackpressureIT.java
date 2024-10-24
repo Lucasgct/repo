adb1c1a88bc2788381bf38387a97599d75fa766e
Ruirui Zhang <mariazrr@amazon.com>
2024-10-23 17:54:03 -0700

    address comments Signed-off-by: Ruirui Zhang <mariazrr@amazon.com>

--- server/src/internalClusterTest/java/org/opensearch/search/backpressure/SearchBackpressureIT.java
+++ server/src/internalClusterTest/java/org/opensearch/search/backpressure/SearchBackpressureIT.java
@@ -333,7 +333,7 @@ public class SearchBackpressureIT extends ParameterizedStaticSettingsOpenSearchI
             latch.countDown();
         }
 
-        private Exception getException() {
+        public Exception getException() {
             return exception;
         }
 
@@ -353,7 +353,7 @@ public class SearchBackpressureIT extends ParameterizedStaticSettingsOpenSearchI
         return () -> description;
     }
 
-    interface TaskFactory<T extends Task> {
+    public interface TaskFactory<T extends Task> {
         T createTask(long id, String type, String action, String description, TaskId parentTaskId, Map<String, String> headers);
     }
 
